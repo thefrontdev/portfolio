a8da00afcfede38c34c6b8ed3bc2f957b2fb713d
emmanuel4 <emmanuel_bernalv@hotmail.com>
2023-10-24 03:56:01 -0600

    change site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://thefront.dev',
+  site: 'https://thefrontdev.github.io',
   base: '/portfolio',
 });
